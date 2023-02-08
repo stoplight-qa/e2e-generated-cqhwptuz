99717c45613ce9774128fdbe2d61514f24249161
qa-vxwbi-4-qtbv@example.com <qa-vxwbi-4-qtbv@example.com>
2023-02-08 15:45:38 +0000

    First commit of promoted project

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://x-15182.stoplight-dev.com/api/v1/projects/cHJqOjE3OA/spectral.js?branch_slug=main&token=2eb56cd6-15db-4143-97b7-bbcc54f43b21";
+export default { extends: ruleset };
